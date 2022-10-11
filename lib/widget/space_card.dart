ad9064519f9b62fb3bb9664519fb04e877c90225
Hafizh <Hafizh@gmail.com>
2022-10-11 09:50:03 +0700

    2 commit

--- lib/widget/space_card.dart
+++ lib/widget/space_card.dart
@@ -1,13 +1,14 @@
 // ignore_for_file: sized_box_for_whitespace, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:kost_app/pages/detail_page.dart';
 import 'package:kost_app/shared/theme.dart';
 
 class SpaceCard extends StatelessWidget {
   final int id;
   final String name;
   final String imageUrl;
-  final double price;
+  final int price;
   final String city;
   final String country;
   final int rating;
@@ -24,91 +25,102 @@ class SpaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(18),
-          child: Container(
-            width: 130,
-            height: 110,
-            child: Stack(
-              children: [
-                Image.asset(
-                  imageUrl,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 70,
-                    height: 30,
-                    decoration: BoxDecoration(
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
+          ),
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(18),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    imageUrl,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
                         color: purpleColor,
                         borderRadius: BorderRadius.only(
                           bottomLeft: Radius.circular(36),
-                        )),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'assets/icon_star.png',
-                          width: 20,
-                          height: 20,
                         ),
-                        Text(
-                          rating.toString(),
-                          style: whiteTextStyle.copyWith(
-                            fontSize: 13,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/icon_star.png',
+                            width: 20,
+                            height: 20,
                           ),
-                        ),
-                      ],
+                          Text(
+                            rating.toString(),
+                            style: whiteTextStyle.copyWith(
+                              fontSize: 13,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              name,
-              style: blackTextStyle.copyWith(
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Text.rich(
-              TextSpan(
-                text: '\$${price}',
-                style: purpleTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                children: [
-                  TextSpan(
-                    text: ' / month',
-                    style: greyTextStyle.copyWith(
-                      fontSize: 16,
-                    ),
-                  )
                 ],
               ),
             ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              '${city}, ${country}',
-              style: greyTextStyle,
-            ),
-          ],
-        )
-      ],
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: '\$${price}',
+                  style: purpleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: ' / month',
+                      style: greyTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${city}, ${country}',
+                style: greyTextStyle,
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
